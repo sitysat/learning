9cf5a44c926a12c5677f15733067fcbb57599b27
sitysat <sittapong.s@gmail.com>
2013-07-12 23:18:43 +0700

    add notes and test pdb

--- python/main.py
+++ python/main.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python -tt
 # Copyright 2013 novitat.co.th All Rights Reserved.
 
+import pdb
+
+pdb.set_trace()
+
 print 'Hello world'
+
+myList = range(10)
+
+for item in myList:
+	print 'loop item {1!s} {0!s}'.format( item, 'MyList' )
+
